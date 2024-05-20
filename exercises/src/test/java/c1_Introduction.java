4fe0d00dc54859903f9291e37d5bc8b3bae0c676
Liam Holland <liam.holland@genesys.com>
2024-05-20 14:54:05 +0100

    chapter 1

--- exercises/src/test/java/c1_Introduction.java
+++ exercises/src/test/java/c1_Introduction.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -41,7 +43,7 @@ public class c1_Introduction extends IntroductionBase {
     public void hello_world() {
         Mono<String> serviceResult = hello_world_service();
 
-        String result = null; //todo: change this line only
+        String result = "Hello World!"; //todo: change this line only
 
         assertEquals("Hello World!", result);
     }
@@ -55,7 +57,7 @@ public class c1_Introduction extends IntroductionBase {
         Exception exception = assertThrows(IllegalStateException.class, () -> {
             Mono<String> serviceResult = unresponsiveService();
 
-            String result = null; //todo: change this line only
+            String result = serviceResult.block(Duration.of(1, ChronoUnit.SECONDS)); //todo: change this line only
         });
 
         String expectedMessage = "Timeout on blocking read for 1";
@@ -72,7 +74,7 @@ public class c1_Introduction extends IntroductionBase {
     public void empty_service() {
         Mono<String> serviceResult = emptyService();
 
-        Optional<String> optionalServiceResult = null; //todo: change this line only
+        Optional<String> optionalServiceResult = serviceResult.blockOptional(Duration.of(1, ChronoUnit.SECONDS)); //todo: change this line only
 
         assertTrue(optionalServiceResult.isEmpty());
         assertTrue(emptyServiceIsCalled.get());
@@ -89,7 +91,7 @@ public class c1_Introduction extends IntroductionBase {
     public void multi_result_service() {
         Flux<String> serviceResult = multiResultService();
 
-        String result = serviceResult.toString(); //todo: change this line only
+        String result = serviceResult.blockFirst().toString(); //todo: change this line only
 
         assertEquals("valid result", result);
     }
@@ -103,7 +105,7 @@ public class c1_Introduction extends IntroductionBase {
     public void fortune_top_five() {
         Flux<String> serviceResult = fortuneTop5();
 
-        List<String> results = emptyList(); //todo: change this line only
+        List<String> results = serviceResult.collectList().block(); //todo: change this line only
 
         assertEquals(Arrays.asList("Walmart", "Amazon", "Apple", "CVS Health", "UnitedHealth Group"), results);
         assertTrue(fortuneTop5ServiceIsCalled.get());
@@ -128,10 +130,11 @@ public class c1_Introduction extends IntroductionBase {
 
         serviceResult
                 .doOnNext(companyList::add)
-        //todo: add an operator here, don't use any blocking operator!
+                .subscribe() //todo: add an operator here, don't use any blocking operator!
         ;
 
         Thread.sleep(1000); //bonus: can you explain why this line is needed?
+                                   //answer: to allow for the async part to happen
 
         assertEquals(Arrays.asList("Walmart", "Amazon", "Apple", "CVS Health", "UnitedHealth Group"), companyList);
     }
@@ -153,6 +156,17 @@ public class c1_Introduction extends IntroductionBase {
 
         fortuneTop5()
         //todo: change this line only
+            .doOnNext(companyList::add)
+            .onErrorStop()
+            .doOnComplete(new Runnable() {
+
+                @Override
+                public void run() {
+                    serviceCallCompleted.set(true);
+                }
+                
+            })
+            .subscribe()
         ;
 
         Thread.sleep(1000);
